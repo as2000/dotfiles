f3b1f64190f532df47cb2a5e397378753df9f2d5
Aiden Scott <aidenscott12@googlemail.com>
2018-02-06 23:14:28 +0000

    Create install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt install i3
+sudo apt install git
+sudo apt install tmux
+
+mkdir -p ~/.fonts
+mkdir -p ~/src
+
+git clone https://github.com/banga/powerline-shell ~/src/powerline-shell
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+ln Monaco%20for%20Powerline.ttf ~/.fonts/Monaco.ttf
+ln vimrc ~/.vimrc
+ln i3config ~/.config/i3/config
+ln tmux.conf ~/.tmux.conf
+ln gitconfig-personal ~/.gitconfig
+ln bash_profile ~/.bash_profile
+
+cd ~/src/powerline-shell
+sudo python setup.py install
+source ~/.bash_profile
